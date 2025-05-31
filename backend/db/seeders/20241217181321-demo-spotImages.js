c566d30833f2bd30e1cec5a38111f60e2a62e492
gabesauce <alfaro.gabe@gmail.com>
2025-05-30 17:27:48 -0700

    seeder update

--- backend/db/seeders/20241217181321-demo-spotImages.js
+++ backend/db/seeders/20241217181321-demo-spotImages.js
@@ -50,7 +50,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      url: { [Op.in]: [1, 2, 3, 4, 5, 6] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6] }
     }, {});
   }
 };
