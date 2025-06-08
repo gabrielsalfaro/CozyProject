a0711d8497edc7d74fa21b8492f56770961cc799
gabesauce <alfaro.gabe@gmail.com>
2025-06-07 18:53:30 -0700

    Reviews, Nav zIndex, css update

--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -2,14 +2,23 @@ import { FaRegStar } from "react-icons/fa";
 import CreateNewReview from "../CreateNewReview/CreateNewReview";
 import OpenModalButton from '../OpenModalButton';
 import React from 'react';
-
+import { useSelector } from 'react-redux'
 import './Reviews.css';
 
 function Reviews({ reviews = [] }) {
+  const sessionUser = useSelector(state => state.session.user);
+  const host = useSelector(state => state.spots.singleSpot?.Owner?.id);
+  const hasReviewed = reviews.some(review => review?.userId === sessionUser?.id)
   const averageStars =
     reviews.length > 0
       ? (reviews.reduce((sum, review) => sum + review.stars, 0) / reviews.length).toFixed(1)
       : null;
+
+  // clg full redux state
+  // const state = useSelector(state => state); 
+  // const logState = () => {
+  //   console.log('Full Redux state: ', JSON.stringify(state, null, 2));
+  // };
 
   return (
     <>
@@ -18,11 +27,16 @@ function Reviews({ reviews = [] }) {
         <p className="reviews-rating">
           <FaRegStar /> {averageStars} · {reviews.length} review{reviews.length > 1 ? 's' : ''}
         </p> 
-        <OpenModalButton 
-          buttonText='Post Your Review'
-          className="review-create-review"
-          modalComponent={<CreateNewReview />}
-        >Post Your Review</OpenModalButton>
+
+        {/* double check (sessionUser != host) */}
+        {sessionUser && (sessionUser != host) && (!hasReviewed) && (
+          <OpenModalButton 
+            buttonText='Post Your Review'
+            className="review-create-review"
+            modalComponent={<CreateNewReview />}
+          >Post Your Review</OpenModalButton>
+          
+         )}
       </>)}
       
         {reviews.length === 0 ? (
@@ -54,6 +68,7 @@ function Reviews({ reviews = [] }) {
           })
         )}
       </div>
+      {/* <button onClick={logState}> debug: state </button> */}
     </>
   );
 }
